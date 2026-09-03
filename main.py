54dab9226fb6dafd573f545c7761c61499e74780
русiк <rkulasov749@gmail.com>
2026-09-03 20:38:05 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://cipo4kin.github.io"],
+    allow_credentials=False,
+    allow_methods=["POST"],
+    allow_headers=["Content-Type"],
+)
+
+
 class ContactRequest(BaseModel):
     name: str
     phone: str
     comment: str
+
+
 @app.post("/contact")
 def send_contact(request: ContactRequest):
     return {"message": "Заявка отправлена"}
